e9a8d9f98528eb48f62b9be9317d1430bd9c968b
Gustavo Lara <37640466+gustavolara@users.noreply.github.com>
2018-06-19 11:34:55 -0300

    Pula em Ordem(Não termina corrida)

--- src/kanguru/Canguru.java
+++ src/kanguru/Canguru.java
@@ -3,10 +3,10 @@ package kanguru;
 public class Canguru extends Thread{
     private int DistPercorrida;
     private int DistMaxPercorrida;
-    private boolean chegou;
+    public boolean chegou;
     private Corrida corrida;
     public int id;
-    public boolean pula;
+    //public boolean pula;
     
     public Canguru(String nome,Corrida corrida,int id){
         super(nome);
@@ -24,48 +24,17 @@ public class Canguru extends Thread{
     }
     public void setDistMaxPercorrida(int DistMaxPercorrida) {
         this.DistMaxPercorrida = DistMaxPercorrida;
-    }
-    
-    public synchronized void Pula(){
-        int pulou;
-        setDistPercorrida((int)(Math.random()*10)+1);
-        pulou = getDistMaxPercorrida();
-        pulou += getDistPercorrida();
-        setDistMaxPercorrida(pulou);
-        System.out.println("Nome: "+Thread.currentThread().getName()+"\nPulou : "
-                + getDistPercorrida()+ "\nTotal percorrido : "+ getDistMaxPercorrida()+"\n");
-        if(this.DistMaxPercorrida >= this.corrida.tamCorrida){
-            chegou = true;
-            corrida.chegada(this.id);
-        }
-        pula = true;
-        notify();
-    }
-    
-    public synchronized void espera(){
-        
-        while(!this.pula){
-            try {
-                wait();
-            } catch (InterruptedException ex) {
-                ex.printStackTrace();
-            }
-        }
-        this.pula = false;
-    }    
+    }  
  
     @Override
     public void run(){
         while(!chegou){
-            Pula();
+            corrida.Pula(this);
             try {
-                Thread.sleep(100);
-            } catch (InterruptedException ex) {
+                corrida.espera(this);
+            } catch (Exception ex) {
                 ex.printStackTrace();
             }
-            espera();
         }
-        
-        
     }
 }
--- src/kanguru/Corrida.java
+++ src/kanguru/Corrida.java
@@ -5,6 +5,7 @@ public class Corrida {
     public int tamCorrida = ((int)(Math.random()*20)+80);
     public boolean acabou=false;
     int aux;
+    private int ordem;
 
     public synchronized void chegada(int canguru){
         this.colocacao[aux] = canguru;
@@ -12,12 +13,44 @@ public class Corrida {
         if(aux==5){
             acabou=true;
             podium();
-        }
-        
+        }   
     } 
+    
     public void podium(){
-        System.out.println("Órdem de Chegada:");
+        System.out.println("Ordem de Chegada:");
         for(int i=0;i<5;i++)
             System.out.println("Jack "+this.colocacao[i]);
+    }
+    
+    public synchronized void Pula(Canguru canguru){
+        int pulou;
+        canguru.setDistPercorrida((int)(Math.random()*10)+1);
+        pulou = canguru.getDistMaxPercorrida();
+        pulou += canguru.getDistPercorrida();
+        canguru.setDistMaxPercorrida(pulou);
+        System.out.println("Nome: "+Thread.currentThread().getName()+"\nPulou : "
+                + canguru.getDistPercorrida()+ "\nTotal percorrido : "+ canguru.getDistMaxPercorrida()+"\n");
+        if(canguru.getDistMaxPercorrida() >= this.tamCorrida){
+            canguru.chegou = true;
+            this.chegada(canguru.id);
+        }
+        //canguru.pula = true;
+        //if(canguru.id==5)
+            notifyAll();
+    }
+    
+    
+    
+    
+    public synchronized void espera(Canguru canguru){
+        this.ordem = (this.ordem+1)%5;
+        while((canguru.id-1) != this.ordem){
+            try {
+                wait();
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+        //canguru.pula = false;
     }
 }
